9fed8767aa78058730af81a4ce31a3533bd693ae
Lasitha <lasitha@gatech.edu>
2020-02-12 14:05:58 -0500

    reorganized the code for dual arm

--- full_arm_optimization_twists/Dual Arm/dual_arm.m
+++ full_arm_optimization_twists/Dual Arm/dual_arm.m
@@ -28,15 +28,16 @@ J            = computeJacobian(q);
 % t = W\J'/(J/W*J')*[se3ToVec(V_R);se3ToVec(V_L)];
 
 %% main script
-eomg = 0.01;
-ev = 0.001;
+eomg = 0.001;
+ev = 0.0001;
 % the delta that needs to move
-T_delta_R    = RpToTrans(eye(3),[0.04,0.0,0.05]');
-T_delta_L    = RpToTrans(eye(3),[0.0,0.0,0.05]');
+T_delta_R    = RpToTrans(eye(3),[0.0,0.0,0.05]');
+T_delta_L    = RpToTrans(eye(3),[0.03,0.0,0.05]');
 
-FKinSpace(M_R, Slist_R, theta_mini_R)
+% current positions
+FKinSpace(M_R, Slist_R, theta_mini_R);
 FK_K         = FKinSpace(M_K, Slist_K, theta_KUKA);
-FK_R         = FK_K * FKinSpace(M_R, Slist_R, theta_mini_R)
+FK_R         = FK_K * FKinSpace(M_R, Slist_R, theta_mini_R);
 FK_L         = FK_K * FKinSpace(M_L, Slist_L, theta_mini_L);
 
 % compute the spatial velocity from velocity
@@ -44,16 +45,26 @@ v_L = [0,0,0]'; v_R = [0,0,0]';
 w_L = [0,0,0]'; w_R = [0,0,0]';
 [V_L, V_R] = computeSpatialVel(v_L, w_L, v_R, w_R, FK_R, FK_L);
 
-
-T_d_R      = FK_R * T_delta_R
-T_d_L      = FK_L * T_delta_L
+% here, compute the final pose after moving the amount defined by
+% T_delta_R and T_delta_L
+T_d_R      = FK_R * T_delta_R;
+T_d_L      = FK_L * T_delta_L;
 
 
 % [thetalist, success] = IKinSpace(Slist_K, M_K, M_R, Slist_R, M_L, Slist_L, T_d_R, T_d_L, q, eomg, ev);
-[thetalist,L,R] = ikine_dual(Slist_K, M_K, M_R, Slist_R, M_L, Slist_L, T_d_R, T_d_L, q, eomg, ev)
+[thetalist,L,R] = ikine_dual(Slist_K, M_K, M_R, Slist_R, M_L, Slist_L, T_d_R, T_d_L, q, eomg, ev);
 
-% evaluate the result
-% FKinSpace(M_K, Slist_K, thetalist)
+%% evaluate the result
+theta_KUKA = thetalist(1:7);
+theta_L    = thetalist(8:14);
+theta_R    = thetalist(15:21);
+
+FK_K         = FKinSpace(M_K, Slist_K, theta_KUKA);
+FK_R         = FK_K * FKinSpace(M_R, Slist_R, theta_R);
+FK_L         = FK_K * FKinSpace(M_L, Slist_L, theta_L);
+
+err_L        = T_d_R - FK_R
+err_R        = T_d_L - FK_L
 
 %% inverse differential kinematics for the dual arm
 function [thetalist,err_val_n_L,err_val_n_R] = ikine_dual(Slist_K, M_K, M_R, Slist_R, M_L, Slist_L, T_d_R, T_d_L, thetalist0, eomg, ev)
@@ -61,7 +72,7 @@ function [thetalist,err_val_n_L,err_val_n_R] = ikine_dual(Slist_K, M_K, M_R, Sli
     % differential kinematics
     thetalist = thetalist0;
     i = 0;
-    maxiterations = 100;
+    maxiterations = 200;
     
     theta_mini_R = thetalist0(15:21);
     theta_mini_L = thetalist0(8:14);
