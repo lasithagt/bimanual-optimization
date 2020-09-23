f3003c4f62d70bd1830901056f880bbef769c052
Lasitha <lasithagt@gmail.com>
2020-09-23 13:34:29 -0400

    updates to visulize and plot raw data

--- Motion Characterization/plot_data.m
+++ Motion Characterization/plot_data.m
@@ -1,13 +1,12 @@
 
 function plot_data(em_data, is_animate)
     %% plotting
-    % figure(3)
     % TODO: Extract force data at each point. (for visualization)
     % close all;
     
     line = {'r-','k-'};
-    q_ = {'r','b'};
-    r = 10;
+    q_   = {'r','b'};
+    r    = 10;
 
     numsen = size(em_data,3);
     n_data = size(em_data,2);
@@ -20,7 +19,7 @@ function plot_data(em_data, is_animate)
     zlabel('z position')
 
     grid on
-    h = [h1,h2];
+    h = [h1, h2];
     
     for i = 1:numsen
         if (is_animate)
@@ -28,7 +27,7 @@ function plot_data(em_data, is_animate)
                 if mod(j,r) == 0
                     plot3(em_data(1,j,i), em_data(2,j,i), em_data(3,j,i),'.')
                     quiver3(em_data(1,j,i),em_data(2,j,i),em_data(3,j,i), ...
-                        -em_data(4,j,i),-em_data(5,j,i),-em_data(6,j,i),q_{i}, 'LineWidth',0.5,'MaxHeadSize',0.05)
+                        -em_data(4,j,i),-em_data(5,j,i), -em_data(6,j,i),q_{i}, 'LineWidth',0.5,'MaxHeadSize',0.05)
                     hold on
                     % axis([0 30 -10 10 -10 25])
                     pause(0.01)
@@ -37,7 +36,7 @@ function plot_data(em_data, is_animate)
         else
             plot3(em_data(1,:,i), em_data(2,:,i), em_data(3,:,i),'.')
             quiver3(em_data(1,:,i),em_data(2,:,i),em_data(3,:,i), ...
-                -em_data(4,:,i),-em_data(5,:,i),-em_data(6,:,i),q_{i}, 'LineWidth',0.5,'MaxHeadSize',0.05)
+                em_data(4,:,i),em_data(5,:,i), em_data(6,:,i),q_{i}, 'LineWidth',0.5,'MaxHeadSize',0.05)
             hold on
             % axis([0 30 -10 10 -10 25])
         end
--- Motion Characterization/visualize_data.m
+++ Motion Characterization/visualize_data.m
@@ -6,7 +6,7 @@ clear;
 % Load the data from a mat file
 calibration = 1;
 if (calibration)
-    cali_data = readNPY('raw_data/EM_tracker/calibration_data_path_tracking_2.npy');
+    cali_data = readNPY('raw_data/EM_tracker/calibration_data_path_tracking.npy');
     ws_d      = rosbag('raw_data/EM_tracker/path_tracking.bag');
 
 
@@ -63,6 +63,7 @@ if (calibration)
         % em_data_mat_ang = quat2eul(em_data_mat(4:end,:,i)');
         em_data_mat_ang = em_data_mat(4:end-1,:,i)';
         
+        % global rotation matrix
         rotm_global = rotx(-pi) * rotz(pi);
 
         for k = 1:n_data
@@ -89,40 +90,10 @@ end
 
 %% plotting
 figure(3)
-% % TODO: Extract force data at each point. (for visualization)
-% % close all;
-% line = {'r-','k-'};
-% q_ = {'r','b'};
-% r = 10;
-% 
-% numsen = size(em_data_adj,3);
-% n_data = size(em_data_adj,2);
-% h1 = animatedline('Marker','.','Color','r');
-% h2 = animatedline('Marker','.','Color','k');
-% 
-% 
-% xlabel('x position')
-% ylabel('y position')
-% zlabel('z position')
-% 
-% grid on
-% h = [h1,h2];
-% for j=1:n_data
-%     j;
-%     for i=1:numsen
-%         if mod(j,r) == 0
-%             
-%             plot3(em_data_adj(1,j,i), em_data_adj(2,j,i), em_data_adj(3,j,i),'.')
-%             quiver3(em_data_adj(1,j,i),em_data_adj(2,j,i),em_data_adj(3,j,i), ...
-%                 -em_data_adj(4,j,i),-em_data_adj(5,j,i),-em_data_adj(6,j,i),q_{i}, 'LineWidth',0.5,'MaxHeadSize',0.05)
-%             hold on
-% %             axis([0 30 -10 10 -10 25])
-%             pause(0.01)
-%         end
-%     end
-% end
-plot_data(em_data_adj, false)
+is_animate = false;
+plot_data(em_data_adj, is_animate)
 
+%% plotting velocities
 figure(4)
 % TODO: Extract force data at each point. (for visualization)
 % close all;
